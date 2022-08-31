80c1a90208044fb84d46ad8516b3a09778babdc4
kelchan <kelvin.chan131@gmail.com>
2022-08-31 12:17:47 -0700

    used props to display

--- src/components/PersonCard.js
+++ src/components/PersonCard.js
@@ -1,30 +1,44 @@
-import React, { Component } from "react";
+// import React, { Component } from "react";
 
-class PersonCard extends Component {
-    constructor( props ) {
-        super( props );
-        this.state = {
-            Age : props.Age
-        };
-    }
+// class PersonCard extends Component {
+//     constructor( props ) {
+//         super( props );
+//         this.state = {
+//             Age : props.Age
+//         };
+//     }
 
-    birthdayButton = () => {
-        let newAge = this.state.Age += 1;
-        this.setState( { Age : newAge } );
-        console.log( newAge );
-    }
+//     birthdayButton = () => {
+//         let newAge = this.state.Age += 1;
+//         this.setState( { Age : newAge } );
+//         console.log( newAge );
+//     }
 
-    render() {
-        const { firstName, lastName, Age, HairColor } = this.props;
-        return (
-            <div>
-                <h1>{ lastName }, { firstName }</h1>
-                <p>Age: { this.state.Age }</p>
-                <p>Hair Color: { HairColor }</p>
-                <button onClick = { this.birthdayButton } > Birthday Button for { firstName } { lastName } </button>
-            </div>
-        );
-    }
+//     render() {
+//         const { firstName, lastName, Age, HairColor } = this.props;
+//         return (
+//             <div>
+//                 <h1>{ lastName }, { firstName }</h1>
+//                 <p>Age: { this.state.Age }</p>
+//                 <p>Hair Color: { HairColor }</p>
+//                 <button onClick = { this.birthdayButton } > Birthday Button for { firstName } { lastName } </button>
+//             </div>
+//         );
+//     }
+// }
+
+// export default PersonCard;
+
+
+import React from 'react';
+const PersonCard = props => {
+    return(
+        <div>
+            <h1>{ props.lastName }, { props.firstName }</h1>
+            <p>Age: { props.Age }</p>
+            <p>Hair Color: { props.HairColor }</p>
+        </div>
+    );
 }
-
 export default PersonCard;
+
